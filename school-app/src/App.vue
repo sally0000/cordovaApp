6970a6efcc8a6157c5cdedf42f21ff57128d7bfa
sally0000 <13386251970@163.com>
2021-01-13 09:55:37 +0800

    提交cordova项目

---
+++ school-app/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <div>
+      <p>
+        If iView is successfully added to this project, you'll see an
+        <code v-text="'<Button>'"></code>
+        below
+      </p>
+      <van-button type="primary">Button</van-button>
+      <van-tabbar v-model="active">
+        <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+        <van-tabbar-item icon="search">标签</van-tabbar-item>
+        <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+        <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+      </van-tabbar>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      active: 0,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
